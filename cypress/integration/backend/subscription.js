1f4e491576b6250b610c920437b6c9c8bee34e09
Lucas Fernandes <lfsilva.ext@vr.com.br>
2021-02-19 16:03:18 -0300

    update scenario name

--- cypress/integration/backend/subscription.js
+++ cypress/integration/backend/subscription.js
@@ -1,5 +1,5 @@
-describe('Search Subscrition successfully', () => {
-    it('Positive scenario', () => {
+describe('Subscription Test', () => {
+    it('Search Subscrition successfully', () => {
         cy.request('GET', 'subscription')
         .then((response) => {
             expect(response.status).to.eq(200);
